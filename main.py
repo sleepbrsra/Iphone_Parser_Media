c679bb92963704d8844bdad91ebfb23a5f24a886
sleepbrsra <kilyokqe@gmail.com>
2024-10-03 21:00:11 +0300

    123

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def print_ascii():
 ⡞⠀⠀⠸⠁⠀⠀⠀⠀⠀⠀⠀⢳⢀⣠⠀⠀⠀⠉⠉⠀⠀⣀⠀⠀⠀⢀⣠⡴⠞⠁⠀⠀⠈⠓⠦⣄⣀⠀⠀⠀⠀⣀⣤⠞⠁⠀                                     
 ⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠀⠁⠀⢀⣀⣀⡴⠋⢻⡉⠙⠾⡟⢿⣅⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠙⠛⠉⠉⠀⠀⠀⠀
 ⠘⣦⡀⠀⠀⠀⠀⠀⠀⣀⣤⠞⢉⣹⣯⣍⣿⠉⠟⠀⠀⣸⠳⣄⡀⠀⠀⠙⢧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠈⠙⠒⠒⠒⠒⠚⠋⠁⠀⡴⠋⢀⡀⢠⡇⠀⠀⠀⠀⠃⠀⠀⠀⠀⠀⢀⡾⠋⢻⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠈⠙⠒⠒⠒⠒⠒⠚⠋⠁⠀⡴⠋⢀⡀⢠⡇⠀⠀⠀⠀⠃⠀⠀⠀⠀⠀⢀⡾⠋⢻⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⢸⡀⠸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⢠⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣇⠀⠀⠉⠋⠻⣄⠀⠀⠀⠀⠀⣀⣠⣴⠞⠋⠳⠶⠞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⠦⢤⠤⠶⠋⠙⠳⣆⣀⣈⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -85,34 +85,60 @@ def connect_iphone():
     print("iPhone успешно подключен и смонтирован.")
 
 
+def copy_with_unique_name(src, dst):
+    """Копирование файла с добавлением суффикса, если файл с таким именем уже существует."""
+    if not os.path.exists(dst):
+        shutil.copy2(src, dst)
+    else:
+        base, extension = os.path.splitext(dst)
+        counter = 1
+        new_dst = f"{base}_{counter}{extension}"
+        while os.path.exists(new_dst):
+            counter += 1
+            new_dst = f"{base}_{counter}{extension}"
+        shutil.copy2(src, new_dst)
+        print(f"Файл с таким же именем найден. Новый файл сохранён как {new_dst}")
+
+
 def download_photos(file_type):
     connect_iphone()
     iphone_directory = '~/iphone'  # Путь к директории с фото
-    download_directory = './downloaded_photos'  # Путь, куда будут скачиваться фото
+    download_directory = './downloaded_photos'  # Путь, куда будут скачиваться файлы
 
-    os.makedirs(download_directory, exist_ok=True)  # Создаем папку для скачивания, если она не существует
+    # Создаем все подкаталоги сразу, чтобы избежать ошибок
+    subdirs = {
+        'photos': os.path.join(download_directory, 'photos'),
+        'videos': os.path.join(download_directory, 'videos'),
+        'gifs': os.path.join(download_directory, 'gifs'),
+        'others': os.path.join(download_directory, 'others')
+    }
+
+    for subdir in subdirs.values():
+        os.makedirs(subdir, exist_ok=True)  # Создаем подкаталоги для всех типов файлов
+
+    # Определяем расширения файлов для разных категорий
+    extensions = {
+        'photos': ('.jpg', '.jpeg', '.png', '.heic'),
+        'videos': ('.mp4', '.mov'),
+        'gifs': ('.gif'),
+    }
 
     if file_type == 'all':
-        subdirs = {
-            'photos': os.path.join(download_directory, 'photos'),
-            'gifs': os.path.join(download_directory, 'gifs'),
-            'videos': os.path.join(download_directory, 'videos'),
-            'others': os.path.join(download_directory, 'others')
-        }
-        for subdir in subdirs.values():
-            os.makedirs(subdir, exist_ok=True)  # Создаем подкаталоги
-
-        extensions = {
-            'photos': ('.jpg', '.jpeg', '.png', '.gif', '.heic', '.thm'),
-            'videos': ('.mp4', '.mov'),
-        }
+        allowed_extensions = extensions['photos'] + extensions['videos'] + extensions['gifs']
+    elif file_type == 'photos':
+        allowed_extensions = extensions['photos']
+    elif file_type == 'videos':
+        allowed_extensions = extensions['videos']
+    elif file_type == 'gifs':
+        allowed_extensions = extensions['gifs']
     else:
-        extensions = {
-            'photos': ('.jpg', '.jpeg', '.png', '.gif', '.heic', '.thm') if file_type == 'photos' else (),
-            'videos': ('.mp4', '.mov') if file_type == 'videos' else ()
-        }
+        allowed_extensions = ()
 
-    total_photos = sum(count_photos_in_directory(os.path.expanduser(iphone_directory))[0] for ext in extensions.values())
+    # Подсчитываем общее количество файлов для загрузки
+    total_photos = sum(
+        1 for root, dirs, files in os.walk(os.path.expanduser(iphone_directory))
+        for file in files if any(file.lower().endswith(ext) for ext in allowed_extensions)
+    )
     
     if total_photos == 0:
         print("Нет файлов для скачивания.")
@@ -120,19 +146,21 @@ def download_photos(file_type):
         return
 
     print(f"Начинаем скачивание {total_photos} файлов...")
-    
+
     downloaded_photos = 0
 
     for root, dirs, files in os.walk(os.path.expanduser(iphone_directory)):
         for file in files:
-            if any(file.lower().endswith(ext) for ext in extensions['photos']):
-                destination = os.path.join(download_directory, 'photos', file)
-            elif any(file.lower().endswith(ext) for ext in extensions['videos']):
-                destination = os.path.join(download_directory, 'videos', file)
+            if file.lower().endswith(extensions['photos']):
+                destination = os.path.join(subdirs['photos'], file)
+            elif file.lower().endswith(extensions['videos']):
+                destination = os.path.join(subdirs['videos'], file)
+            elif file.lower().endswith(extensions['gifs']):
+                destination = os.path.join(subdirs['gifs'], file)
             else:
-                destination = os.path.join(download_directory, 'others', file)
+                destination = os.path.join(subdirs['others'], file)
 
-            shutil.copy2(os.path.join(root, file), destination)  # Копируем файл
+            copy_with_unique_name(os.path.join(root, file), destination)  # Используем уникальное имя
             downloaded_photos += 1
             progress_bar(downloaded_photos, total_photos)
 
